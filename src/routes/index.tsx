4767254b39e7ec026a81317a93257b599151a9fa
pvictorf <41065334+pvictorf@users.noreply.github.com>
2022-01-30 15:53:21 -0300

    Add export pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Home } from "../pages/Home";
+import * as Page from "../pages";
 
 const routes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Page.Home />} />
         <Route path="*" element={(<h1>Not Found :(</h1>)} />
       </Routes>
     </BrowserRouter>
